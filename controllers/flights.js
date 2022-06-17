665758301582739761d97e3d4e02ff1264f427a8
andywtse <atse4995@gmail.com>
2022-06-17 15:25:49 -0400

    Adds controller and routes for flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,31 +1,96 @@
 import { Flight } from '../models/model.js'
 
-function index(req,res) {
-
+function index(req, res) {
+  Flight.find({})
+    .then(flights => {
+      res.render('flights/index', {
+        title: 'All Flights',
+        flights
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect('/');
+    });
 }
 
-function newFlight(req,res) {
-
+function newFlight(req, res) {
+  res.render('flights/new', {
+    title: "Add Flight"
+  });
 }
 
-function show(req,res) {
-
+function show(req, res) {
+  Flight.findById(req.params.id)
+    .then(flight => {
+      res.render('flights/show', {
+        title: 'Flight Detail',
+        flight
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect('/');
+    });
 }
 
-function edit(req,res) {
-
+function edit(req, res) {
+  Flight.findById(req.params.id)
+    .then(flight => {
+      res.render('flights/edit', {
+        title: 'Edit Flight',
+        flight
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect('/');
+    });
 }
 
-function create(req,res) {
+function create(req, res) {
 
+  for (let key in req.body) {
+    if (req.body[key] === '') {
+      delete req.body[key];
+    }
+  }
+
+  Flight.create(req.body)
+    .then(flight => {
+      res.redirect(`/flights/${flight._id}`)
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect('/flights/new')
+    });
 }
 
-function update(req,res) {
+function update(req, res) {
 
+  for (let key in req.body) {
+    if(req.body[key] === "") delete req.body[key]
+  }
+
+  Flight.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(flight => {
+      res.redirect(`/flights/${flight._id}`);
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect('/')
+    });
 }
 
-function deleteFlight(req,res) {
-  
+function deleteFlight(req, res) {
+  Flight.findByIdAndDelete(req.params.id)
+  .then(flight => {
+    res.redirect('/');
+  })
+  .catch(error => {
+    console.log(error);
+    res.redirect('/')
+  });
 }
 
 export {
